304a436b78eb9b47ec74d1077ff2f562d4281cf4
Jesseyx <jyaoxiao@hotmail.com>
2017-03-13 18:12:42 +0800

    Do server...

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,29 @@
-console.log('server...');
+const http = require('http');
+const crypto = require('crypto');
+
+const constants = require('../constants');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+});
+
+server.on('upgrade', (req, socket) => {
+  let key = req.headers['sec-websocket-key'];
+
+  const shasum = crypto.createHash('sha1');
+  key = shasum.update(key + constants.GUID).digest('base64');
+
+  const headers = [
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: websocket',
+    'Connection: upgrade',
+    'Sec-WebSocket-Accept: ' + key,
+  ];
+
+  // 让数据立即发送
+  socket.setNoDelay(true);
+  socket.write(headers.concat('', '').join('\r\n'));
+});
+
+server.listen(12010);
